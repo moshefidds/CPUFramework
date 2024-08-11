aa9bb692df53b4a778d497ad4edcbc4e505a7449
moshefidds <moshefiddle1234@gmail.com>
2024-08-11 12:33:10 -0400

    initial commit

---
+++ SqlUtility.cs
@@ -0,0 +1,27 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace CPUFramework
+{
+    public class SqlUtility
+    {
+        public static string ConnectionString = "";
+        // GetDataTable - take a sql statement and retaurn a data table
+        public static DataTable GetDataTable(string sqlstatement)
+        {
+            Debug.Print(sqlstatement);
+            DataTable dt = new();
+            SqlConnection conn = new();
+            conn.ConnectionString = ConnectionString;
+            conn.Open();
+
+            var cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sqlstatement;
+            var dr = cmd.ExecuteReader();
+            dt.Load(dr);
+            return dt;
+        }
+    }
+}
